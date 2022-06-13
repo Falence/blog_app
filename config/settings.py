766402b6a11a90db1d6136e137d0d6ba2e236e9e
Falence Lemungoh <falencelemungoh@gmail.com>
2022-06-13 10:27:00 +0100

    added requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = 'django-insecure-95#29*h19@9=7_@i)9atn=)j-rxbg65r8em3*xylct7%zvc0%u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
